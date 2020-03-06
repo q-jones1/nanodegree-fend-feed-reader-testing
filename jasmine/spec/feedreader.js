a1f1e622358d568900dfe03ef3cf8d4bf6f7e541
q-jones1 <56361894+q-jones1@users.noreply.github.com>
2020-03-06 14:25:17 +0000

    feat: Test one entry exists in feed container

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -74,14 +74,25 @@ $(function() {
        });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        /* This test ensures when the loadFeed
+        * function is called and completes its work, there is at least
+        * a single .entry element within the .feed container.
+        * loadFeed() is asynchronous, so this test requires
+        * the use of Jasmine's beforeEach and asynchronous done() function
+        */
+        beforeEach(function(done) {
+          loadFeed(0, function() {
+            done();
+          });
+        });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        it('check at least one entry element exists in the feed container', function(done) {
+          const feedEntry = document.querySelector('.feed .entry');
+          expect(feedEntry).not.toEqual(null);
+          done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
